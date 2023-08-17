f1674e5ef4b9eee7805cdc3182bfb39b78e9c88e
aliciatruong <aliciatruong1125@gmail.com>
2023-08-16 21:47:24 -0700

    Adding auth token builder

--- src/main/java/AuthToken.java
+++ src/main/java/AuthToken.java
@@ -14,7 +14,48 @@ public class AuthToken {
     private String refreshToken;
     private String scope;
 
-    public AuthToken() {
-
+     public String getAccessToken() {
+        return accessToken;
     }
+    public String getTokenType() {
+        return tokenType;
+    }
+    public String getExpiresIn() {
+        return expiresIn;
+    }
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+    public String getScope() {
+        return scope;
+    }
+
+    private AuthToken(AuthBuilder builder) {
+        this.accessToken=builder.accessToken;
+        this.tokenType=builder.tokenType;
+        this.expiresIn=builder.expiresIn;
+        this.refreshToken=builder.refreshToken;
+        this.scope=builder.scope;
+    }
+
+    public static class AuthBuilder() {
+        private String accessToken;
+        private String tokenType;
+        private String expiresIn;
+        private String refreshToken;
+        private String scope;
+
+        public AuthBuilder(String aT, String tT, String eI, String rT, String s) {
+            this.accessToken=aT;
+            this.tokenType=tT;
+            this.expiresIn=eI;
+            this.refreshToken=rT;
+            this.scope=s;
+        }
+
+        public AuthToken build() {
+            return new AuthToken(this);
+        }
+    }
+
 }
