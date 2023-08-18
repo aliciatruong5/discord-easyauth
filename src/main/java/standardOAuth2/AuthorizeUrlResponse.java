54d6da86f7c3c2db5116415725db5ee414897f3e
Clarence <clarrynguyen@gmail.com>
2023-08-17 22:32:34 -0700

    Adding authorize url response starter

---
+++ src/main/java/standardOAuth2/AuthorizeUrlResponse.java
@@ -0,0 +1,8 @@
+package standardOAuth2;
+
+public class AuthorizeUrlResponse {
+    //https://nicememe.website/?code=NhhvTDYsFcdgNLnnLijcl7Ku7bEEeee&state=15773059ghq9183habn
+    private String code;
+    private String state;
+
+}
